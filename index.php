adb5701a1a11937e9e19ec3870d69a8486813708
Niels Bruinsma <niels.bruinsma2002@gmail.com>
2023-03-10 14:09:12 +0100

    Week 1: Front-end

--- index.php
+++ index.php
@@ -5,9 +5,44 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet" href="css/style.css">
-      <title>Home</title>
+      <title>Home | Pannenformkoeken</title>
   </head>
   <body>
-
+    <main class="vh-100 vw-100 flex flex-center">
+      <div class="container vw-80">
+        <ol>
+          <li class="flex flex-center">
+            Beleg 1
+            Beleg 2
+            Beleg 3
+            Hoevaak het voorkomt
+          </li>
+          <li class="flex flex-center">
+            Beleg 1
+            Beleg 2
+            Beleg 3
+            Hoevaak het voorkomt
+          </li>
+          <li class="flex flex-center">
+            Beleg 1
+            Beleg 2
+            Beleg 3
+            Hoevaak het voorkomt
+          </li>
+          <li class="flex flex-center">
+            Beleg 1
+            Beleg 2
+            Beleg 3
+            Hoevaak het voorkomt
+          </li>
+          <li class="flex flex-center">
+            Beleg 1
+            Beleg 2
+            Beleg 3
+            Hoevaak het voorkomt
+          </li>
+        </ol>
+      </div>
+    </main>
   </body>
 </html>
