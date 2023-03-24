4ba07aa445031abcc6f38778c9bfa7fbccd0172f
Niels Bruinsma <niels.bruinsma2002@gmail.com>
2023-03-24 12:47:55 +0100

    Finished front-end

--- edit.php
+++ edit.php
@@ -5,29 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="./css/history.css">
+    <link rel="stylesheet" href="./css/style.css"> 
+    <link rel="stylesheet" href="css/form.css">
     <title>Document</title>
 </head>
 <body>
     <header>
         <a href="./index.php"><i class="fa-solid fa-arrow-right fa-rotate-180" style="color: #ffffff;"></i></a>
     </header>
-    <ol>
-        <li>
-            <div class="text">
-                <p>Naam: Voornaam Achternaam</p>
-                <p>Beleg: Beleg 1</p>
-            </div>
-            <div class="options">
-                <a href="./edit.php?id=1"><button class="btn-edit">
-                    <i class="fa-regular fa-pen-to-square"></i>
-                </button></a>
-                <a href="./delete.php?id=1"><button class="btn-delete">
-                    <i class="fa-sharp fa-solid fa-trash"></i>
-                </button></a>
-            </div>
-        </li>
-    </ol>
+    <main>
+        <h1>Kies uw beleg.</h1>
+        <form action="./code.php" method="POST">
+            <label for="naam">Naam: </label>
+            <input type="text" name="naam" id="naam">
+            <label for="beleg">Beleg: </label>
+            <input type="text" name="beleg" id="beleg">
+            <input type="submit">
+        </form>
+    </main>
 </body>
 </html>
